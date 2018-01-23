9948399e77be09fe28333b31f59a94772e74a097
Samuel Lee <lee.samuel.k@gmail.com>
2018-01-23 14:24:31 -0500

    stuff

--- src/test/java/org/broadinstitute/hellbender/tools/copynumber/ModelSegmentsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/copynumber/ModelSegmentsIntegrationTest.java
@@ -1,10 +1,17 @@
 package org.broadinstitute.hellbender.tools.copynumber;
 
+import org.broadinstitute.hellbender.CommandLineProgramTest;
+
+import java.io.File;
+
 import static org.testng.Assert.*;
 
 /**
  * @author Samuel Lee &lt;slee@broadinstitute.org&gt;
  */
-public final class ModelSegmentsIntegrationTest {
-
+public final class ModelSegmentsIntegrationTest extends CommandLineProgramTest {
+    private static final File TEST_SUB_DIR = new File(toolsTestDir, "copynumber");
+    private static final File TUMOR_DENOISED_COPY_RATIOS_FILE = new File(TEST_SUB_DIR, "model-segments-wes-tumor-denoised-copy-ratios-SM-74P4M-v1-chr20-downsampled.deduplicated.denoisedCR.tsv");
+    private static final File TUMOR_ALLELIC_COUNTS_FILE = new File(TEST_SUB_DIR, "model-segments-wes-tumor-allelic-counts-SM-74P4M-v1-chr20-downsampled.deduplicated.allelicCounts.tsv");
+    private static final File NORMAL_ALLELIC_COUNTS_FILE = new File(TEST_SUB_DIR, "model-segments-wes-normal-allelic-counts-SM-74NEG-v1-chr20-downsampled.deduplicated.allelicCounts.tsv");
 }
