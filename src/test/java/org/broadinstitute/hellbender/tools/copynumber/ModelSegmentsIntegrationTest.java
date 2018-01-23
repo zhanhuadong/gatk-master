02efc5261fe845c14f173285d1b9f409d6694a5d
Samuel Lee <lee.samuel.k@gmail.com>
2018-01-23 11:40:12 -0500

    MultidimensionalKernelSegmenterUnitTest

---
+++ src/test/java/org/broadinstitute/hellbender/tools/copynumber/ModelSegmentsIntegrationTest.java
@@ -0,0 +1,10 @@
+package org.broadinstitute.hellbender.tools.copynumber;
+
+import static org.testng.Assert.*;
+
+/**
+ * @author Samuel Lee &lt;slee@broadinstitute.org&gt;
+ */
+public final class ModelSegmentsIntegrationTest {
+
+}
